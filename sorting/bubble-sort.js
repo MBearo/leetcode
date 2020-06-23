d25c93ff3aafc9b1f370617f7fa452714bc44024
MBearo <295290708@qq.com>
2020-06-23 10:24:04 +0800

    UPG

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -11,4 +11,16 @@ function bubbleSort(array) {
   }
   return array
 }
+//改进后的
+// function bubbleSort(array) {
+//   const { length } = array
+//   for (let i = 0; i < length; i++) {
+//     for (let j = 0; j < length - 1 - i; j++) {
+//       if (array[j] > array[j + 1]) {
+//         swap(array, j, j + 1)
+//       }
+//     }
+//   }
+//   return array
+// }
 console.log(bubbleSort([5,3,5,71,3,4,6]))
---
+++ sorting/select-sort.js
@@ -0,0 +1,20 @@
+const { swap } = require("../util")
+
+function selectSort(array) {
+  const { length } = array
+  let indexMin
+  for (let i = 0; i < length - 1; i++) {
+    indexMin = i
+    //找最小值
+    for (let j = i; j < length; j++) {
+      if (array[indexMin] > array[j]) {
+        indexMin = j
+      }
+    }
+    if (i !== indexMin) {
+      swap(array, i, indexMin)
+    }
+  }
+  return array
+}
+console.log(selectSort([4, 24, 6, 7, 3, 5, 2, 87]))
